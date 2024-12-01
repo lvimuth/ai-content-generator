4bf535b303130a647254682b28a379e3473e4269
Lakshitha Vimuth <lvimuthfb@gmail.com>
2024-12-01 11:21:20 +0400

    side bar

--- app/dashboard/_components/SideNav.tsx
+++ app/dashboard/_components/SideNav.tsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import { History, Home, Settings, WalletCards } from "lucide-react";
+import Image from "next/image";
+import React from "react";
 
 function SideNav() {
+  const MenuList = [
+    {
+      name: "Home",
+      icon: Home,
+      path: "/dashboard",
+    },
+    {
+      name: "History",
+      icon: History,
+      path: "/dashboard/history",
+    },
+    {
+      name: "Billing",
+      icon: WalletCards,
+      path: "/dashboard/billing",
+    },
+    {
+      name: "Settings",
+      icon: Settings,
+      path: "/dashboard/settings",
+    },
+  ];
   return (
-    <div>SideNav</div>
-  )
+    <div className="h-screen p-5 shadow-sm border">
+      <div className="flex justify-center border-b pb-2">
+        <Image src={"/logo.svg"} alt="logo" width={"120"} height={"80"} />
+      </div>
+      <div className="mt-10">
+        {MenuList.map((menu, index) => (
+          <div className="flex gap-2 mb-2 p-3 hover:bg-primary hover:text-white rounded-lg cursor-pointer transition-all">
+            <menu.icon />
+            <h2>{menu.name}</h2>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default SideNav
+export default SideNav;
