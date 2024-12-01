e300f462d377617c4a403e39351e119296e69ec1
Lakshitha Vimuth <lvimuthfb@gmail.com>
2024-12-01 11:42:01 +0400

    side bar

--- app/dashboard/_components/SideNav.tsx
+++ app/dashboard/_components/SideNav.tsx
@@ -1,8 +1,12 @@
+"use client";
 import { History, Home, Settings, WalletCards } from "lucide-react";
 import Image from "next/image";
+import { usePathname } from "next/navigation";
 import React from "react";
 
 function SideNav() {
+  const path = usePathname();
+
   const MenuList = [
     {
       name: "Home",
@@ -27,12 +31,17 @@ function SideNav() {
   ];
   return (
     <div className="h-screen p-5 shadow-sm border">
-      <div className="flex justify-center border-b pb-2">
+      <div className="flex justify-center ">
         <Image src={"/logo.svg"} alt="logo" width={"120"} height={"80"} />
       </div>
+      <hr className="my-4" />
       <div className="mt-10">
         {MenuList.map((menu, index) => (
-          <div className="flex gap-2 mb-2 p-3 hover:bg-primary hover:text-white rounded-lg cursor-pointer transition-all">
+          <div
+            className={`flex gap-2 mb-2 p-3 hover:bg-primary hover:text-white rounded-lg cursor-pointer transition-all items-center ${
+              path == menu.path && "bg-primary text-white"
+            }`}
+          >
             <menu.icon />
             <h2>{menu.name}</h2>
           </div>
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,9 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Outfit } from "next/font/google";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+const outfit = Outfit({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -27,11 +18,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        >
-          {children}
-        </body>
+        <body className={outfit.className}>{children}</body>
       </html>
     </ClerkProvider>
   );
