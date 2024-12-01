2c2202eb693714912541abf6e263e8ce7db2b179
Lakshitha Vimuth <lvimuthfb@gmail.com>
2024-12-01 13:24:40 +0400

    display template

--- app/dashboard/_components/TemplateList.tsx
+++ app/dashboard/_components/TemplateList.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import Templates from "@/app/(data)/Templates";
+import React from "react";
+import TemplateCard from "./TemplateCard";
+
+export interface TEMPLATE {
+  name: string;
+  desc: string;
+  icon: string;
+  category: string;
+  slug: string;
+  aiPrompt: string;
+  form?: FORM[];
+}
+
+export interface FORM {
+  label: string;
+  field: string;
+  name: string;
+  required?: boolean;
+}
 
 function TemplateList() {
   return (
-    <div>TemplateList</div>
-  )
+    <div className="grid grid-cols-2 lg:grid-cols-4 gap-5 p-10">
+      {Templates.map((item: TEMPLATE, index: number) => (
+        <TemplateCard {...item} />
+      ))}
+    </div>
+  );
 }
 
-export default TemplateList
+export default TemplateList;
