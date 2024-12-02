c3fb25034e715f45c343cd4d097d147d46d9fadb
Lakshitha Vimuth <lvimuthfb@gmail.com>
2024-12-02 21:10:25 +0400

    text editors

--- app/dashboard/content/_components/OutputSection.tsx
+++ app/dashboard/content/_components/OutputSection.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import "@toast-ui/editor/dist/toastui-editor.css";
+import { Editor } from "@toast-ui/react-editor";
+import { Button } from "@/components/ui/button";
+import { Copy } from "lucide-react";
 
 function OutputSection() {
   return (
-    <div>OutputSection</div>
-  )
+    <div className="bg-white shadow-lg border">
+      <div className="flex justify-between items-center p-5">
+        <h2>Your Result</h2>
+        <Button>
+          <Copy />
+          Copy
+        </Button>
+      </div>
+      <Editor
+        initialValue="hello react editor world!"
+        previewStyle="vertical"
+        height="600px"
+        initialEditType="markdown"
+        useCommandShortcut={true}
+      />
+    </div>
+  );
 }
 
-export default OutputSection
+export default OutputSection;
