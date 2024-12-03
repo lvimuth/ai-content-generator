4d90203046a4ad88c404973c6d6cdd83663f222b
Lakshitha Vimuth <lvimuthfb@gmail.com>
2024-12-03 15:56:11 +0400

    final

--- app/dashboard/_components/Header.tsx
+++ app/dashboard/_components/Header.tsx
@@ -1,3 +1,4 @@
+import { UserButton } from "@clerk/nextjs";
 import { Search } from "lucide-react";
 import React from "react";
 
@@ -8,10 +9,11 @@ function Header() {
         <Search />
         <input type="text" placeholder="Search" className="outline-none" />
       </div>
-      <div>
+      <div className="flex gap-5 items-center justify-center">
         <h2 className="bg-primary p-1 rounded-full text-xs text-white px-2">
           Join Membership just for $0.99/Month
         </h2>
+        <UserButton />
       </div>
     </div>
   );
